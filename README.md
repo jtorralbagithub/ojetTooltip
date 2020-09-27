27bd3b95da494aed51cd7db4316322a00acc548e
Joseph Torralba <josephtorralba@Josephs-MacBook-Pro.local>
2020-09-27 15:21:38 -0700

    update project to OJET 9.1.0

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Oracle JET Starter App w/ Tooltip Example
 
 A starter OJET app including a Tooltip Example.  Plan is to continue adding/enhancing this app with more examples.
+OJET version: 9.1.0
+
+1. npm install -g @oracle/ojet-cli 
+2. ojet build
+3. ojet serve
--- src/js/appController.js
+++ src/js/appController.js
@@ -1,90 +1,69 @@
 /**
  * @license
- * Copyright (c) 2014, 2019, Oracle and/or its affiliates.
- * The Universal Permissive License (UPL), Version 1.0
+ * Copyright (c) 2014, 2020, Oracle and/or its affiliates.
+ * Licensed under The Universal Permissive License (UPL), Version 1.0
+ * as shown at https://oss.oracle.com/licenses/upl/
+ * @ignore
  */
 /*
  * Your application specific code will go here
  */
-define(['ojs/ojcore', 'knockout', 'ojs/ojmodule-element-utils', 'ojs/ojmodule-element', 'ojs/ojrouter', 'ojs/ojknockout', 'ojs/ojarraytabledatasource',
-  'ojs/ojoffcanvas'],
-  function(oj, ko, moduleUtils) {
+define(['knockout', 'ojs/ojmodule-element-utils', 'ojs/ojresponsiveutils', 'ojs/ojresponsiveknockoututils', 'ojs/ojcorerouter', 'ojs/ojmodulerouter-adapter', 'ojs/ojknockoutrouteradapter', 'ojs/ojurlparamadapter', 'ojs/ojarraydataprovider', 'ojs/ojknockouttemplateutils', 'ojs/ojmodule-element', 'ojs/ojknockout'],
+  function(ko, moduleUtils, ResponsiveUtils, ResponsiveKnockoutUtils, CoreRouter, ModuleRouterAdapter, KnockoutRouterAdapter, UrlParamAdapter, ArrayDataProvider, KnockoutTemplateUtils) {
      function ControllerViewModel() {
-       var self = this;
+
+        this.KnockoutTemplateUtils = KnockoutTemplateUtils;
+
+        // Handle announcements sent when pages change, for Accessibility.
+        this.manner = ko.observable('polite');
+        this.message = ko.observable();
+
+        announcementHandler = (event) => {
+          this.message(event.detail.message);
+          this.manner(event.detail.manner);
+      };
+
+      document.getElementById('globalBody').addEventListener('announce', announcementHandler, false);
 
       // Media queries for repsonsive layouts
-      var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
-      self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
-      var mdQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
-      self.mdScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
+      const smQuery = ResponsiveUtils.getFrameworkQuery(ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
+      this.smScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
 
-       // Router setup
-       self.router = oj.Router.rootInstance;
-       self.router.configure({
-         'Tooltip': {label: 'Tooltip', isDefault: true}
-       });
-      oj.Router.defaults['urlAdapter'] = new oj.Router.urlParamAdapter();
-
-      self.moduleConfig = ko.observable({'view':[], 'viewModel':null});
-
-      self.loadModule = function() {
-        ko.computed(function() {
-          var name = self.router.moduleConfig.name();
-          var viewPath = 'views/' + name + '.html';
-          var modelPath = 'viewModels/' + name;
-          var masterPromise = Promise.all([
-            moduleUtils.createView({'viewPath':viewPath}),
-            moduleUtils.createViewModel({'viewModelPath':modelPath})
-          ]);
-          masterPromise.then(
-            function(values){
-              self.moduleConfig({'view':values[0],'viewModel':values[1]});
-            }
-          );
-        });
-      };
-
-      // Navigation setup
-      var navData = [
-      {name: 'Tooltip', id: 'tooltip',
-       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'}
+      let navData = [
+        { path: '', redirect: 'tooltip' },
+        { path: 'tooltip', detail: { label: 'Tooltip', iconClass: 'oj-ux-ico-bar-chart' } } // ,
+        // { path: 'incidents', detail: { label: 'Incidents', iconClass: 'oj-ux-ico-fire' } },
+        // { path: 'customers', detail: { label: 'Customers', iconClass: 'oj-ux-ico-contact-group' } },
+        // { path: 'about', detail: { label: 'About', iconClass: 'oj-ux-ico-information-s' } }
       ];
-      self.navDataSource = new oj.ArrayTableDataSource(navData, {idAttribute: 'id'});
+      // Router setup
+      let router = new CoreRouter(navData, {
+        urlAdapter: new UrlParamAdapter()
+      });
+      router.sync();
 
-      // Drawer
-      // Close offcanvas on medium and larger screens
-      self.mdScreen.subscribe(function() {oj.OffcanvasUtils.close(self.drawerParams);});
-      self.drawerParams = {
-        displayMode: 'push',
-        selector: '#navDrawer',
-        content: '#pageContent'
-      };
-      // Called by navigation drawer toggle button and after selection of nav drawer item
-      self.toggleDrawer = function() {
-        return oj.OffcanvasUtils.toggle(self.drawerParams);
-      }
-      // Add a close listener so we can move focus back to the toggle button when the drawer closes
-      $("#navDrawer").on("ojclose", function() { $('#drawerToggleButton').focus(); });
+      this.moduleAdapter = new ModuleRouterAdapter(router);
+
+      this.selection = new KnockoutRouterAdapter(router);
+
+      // Setup the navDataProvider with the routes, excluding the first redirected
+      // route.
+      this.navDataProvider = new ArrayDataProvider(navData.slice(1), {keyAttributes: "path"});
 
       // Header
       // Application Name used in Branding Area
-      self.appName = ko.observable("App Name");
+      this.appName = ko.observable("App Name");
       // User Info used in Global Navigation area
-      self.userLogin = ko.observable("john.hancock@oracle.com");
+      this.userLogin = ko.observable("john.hancock@oracle.com");
 
       // Footer
-      function footerLink(name, id, linkTarget) {
-        this.name = name;
-        this.linkId = id;
-        this.linkTarget = linkTarget;
-      }
-      self.footerLinks = ko.observableArray([
-        new footerLink('About Oracle', 'aboutOracle', 'http://www.oracle.com/us/corporate/index.html#menu-about'),
-        new footerLink('Contact Us', 'contactUs', 'http://www.oracle.com/us/corporate/contact/index.html'),
-        new footerLink('Legal Notices', 'legalNotices', 'http://www.oracle.com/us/legal/index.html'),
-        new footerLink('Terms Of Use', 'termsOfUse', 'http://www.oracle.com/us/legal/terms/index.html'),
-        new footerLink('Your Privacy Rights', 'yourPrivacyRights', 'http://www.oracle.com/us/legal/privacy/index.html')
-      ]);
+      this.footerLinks = [
+        {name: 'About Oracle', linkId: 'aboutOracle', linkTarget:'http://www.oracle.com/us/corporate/index.html#menu-about'},
+        { name: "Contact Us", id: "contactUs", linkTarget: "http://www.oracle.com/us/corporate/contact/index.html" },
+        { name: "Legal Notices", id: "legalNotices", linkTarget: "http://www.oracle.com/us/legal/index.html" },
+        { name: "Terms Of Use", id: "termsOfUse", linkTarget: "http://www.oracle.com/us/legal/terms/index.html" },
+        { name: "Your Privacy Rights", id: "yourPrivacyRights", linkTarget: "http://www.oracle.com/us/legal/privacy/index.html" },
+      ];
      }
 
      return new ControllerViewModel();
